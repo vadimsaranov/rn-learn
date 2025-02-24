23442e260aa6ad052dd74c5c1a744bf4a02e8d0b
vadimsaranov <vadim.saranov@ventionteams.com>
2025-02-24 10:11:32 +0200

    Update scripts, add plugin for android intents

--- app.config.ts
+++ app.config.ts
@@ -32,6 +32,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   plugins: [
     'expo-router',
     'expo-secure-store',
+    './plugins/AndroidIntentPlugin.js',
     [
       'expo-splash-screen',
       {
