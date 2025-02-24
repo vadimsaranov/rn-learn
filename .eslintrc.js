952b901b95bad70a401d86ae14e57648eac4ae4a
vadimsaranov <vadim.saranov@ventionteams.com>
2025-02-24 10:31:06 +0200

    Add strict equality comparison instead of loose equality

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   extends: ['expo', 'prettier', 'eslint:recommended'],
   plugins: ['prettier'],
+  ignorePatterns: ['app-example'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'error',
+    'no-eq-null': 'warn',
   },
   env: {
     node: true,
--- app/hooks/useStorageState.ts
+++ app/hooks/useStorageState.ts
@@ -2,7 +2,7 @@ import { useEffect, useCallback, useState } from 'react';
 import * as SecureStore from 'expo-secure-store';
 
 export async function setStorageItemAsync(key: string, value: string | null) {
-  if (value == null) {
+  if (value === null) {
     await SecureStore.deleteItemAsync(key);
   } else {
     await SecureStore.setItemAsync(key, value);
